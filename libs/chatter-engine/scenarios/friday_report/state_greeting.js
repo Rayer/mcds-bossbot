a28166535a672ef2db5e816e8eb276f11dc2fd65
Killercat <rayershih@gmail.com>
2018-09-01 15:31:23 +0800

    Checkpoint

--- libs/chatter-engine/scenarios/friday_report/state_greeting.js
+++ libs/chatter-engine/scenarios/friday_report/state_greeting.js
@@ -4,11 +4,15 @@ function GreetingState() {
 
 GreetingState.prototype.enter_state = function() {
 
-}
+};
 
 GreetingState.prototype.exit_state = function() {
 
-}
+};
+
+GreetingState.prototype.input = function(statement) {
+
+};
 
 
 
